c1e06d0a568ba71179aab275e1385ea273738f97
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2024-02-20 09:46:33 -0600

    Home component completed

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -2,8 +2,25 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 function Home() {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'>
-
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+    <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-pink-600'>Hi, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6] '>Bisrat Mengesha</h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer</h2>
+        <p className=' text-[#8892b0]  max-w-700px py-4'> I'm a full-stack developer specializing in 
+          building web. Currently, I'm focused on 
+          building responsive full-stack web applications.
+        </p>
+        <div>
+        
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+            View Work 
+          < span className='group-hover:rotate-90 duration-300'>
+          <HiArrowNarrowRight className='ml-6'/>
+          </span>
+          </button>
+        </div>
+    </div>
     </div>
   )
 }
